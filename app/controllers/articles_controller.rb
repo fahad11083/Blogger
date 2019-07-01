f38deed6095bef701058ed8e893b9bec46ff9c34
fahad11083 <fahad.anwaar111@gmail.com>
2019-07-01 02:09:12 -0700

    Form based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,32 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
 def index
     @articles=Article.all
 end
 def show
     @articles=Article.find(params[:id])
+end
+def new 
+    @articles=Article.new
+end
+def create
+    @articles =Article.new(article_params)
+  
+    @articles.save
+    redirect_to article_path(@articles)
+end
+def destroy
+    @articles=Article.find(params[:id])
+    @articles.destroy
+    @articles=Article.all
+    redirect_to "/articles"
+end
+def edit
+    @articles=Article.find(params[:id])
+end
+def update
+    @articles = Article.find(params[:id])
+    @articles.update(article_params)
+    redirect_to article_path(@articles)
 end
 end
